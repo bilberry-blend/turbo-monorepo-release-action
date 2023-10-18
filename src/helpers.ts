b6077440e6b347d7eb748ebcd10509e55129f9e6
Snorre Magnus Davøen <snorre.magnus.davoen@adventuretech.no>
2023-10-18 16:51:11 +0200

    feat: GitHub Actions release action for monorepo
    A new GitHub TypeScript Action to help us make releases for the
    Monorepo. Work in progress.
    
    Uses a combination of turborepo, git, and github rest api to figure out
    what the release should be. Works as follows:
    
    1. Loads inputs and tokens for the action and sets up octokit (to use the
       github rest api).
    
    2. Finds the previous release's sha if any. If no release found
       defaults to the previous sha. Should usually not happen.
    
    3. Logs all git commits between release and current sha (the one being
       deployed). List contains sha and subject (heading of git message)
    
    4. Starts processing each commit using turbo to figure out if the
       package/workspace being released was affected by the commit. If
       affected it includes the commit in the list of messages.
    
    5. It processes each commit and groups them by the conventional commit
       type. This allows us to present them more nicely.
    
    6. Finally it builds the release body and creates the release using the
       github api. It also outputs the release title and release body as
       github action outputs. This allows workflows to for exampel post a
       slack message.
    
    This work has not yet been tested in practice. But need some feedback
    before we begin testing it.

---
+++ src/helpers.ts
@@ -0,0 +1,166 @@
+import { exec } from '@actions/exec'
+import * as github from '@actions/github'
+import { Context } from '@actions/github/lib/context'
+import { DryRunJson } from './turbo'
+
+type Octokit = ReturnType<typeof github.getOctokit>
+
+export const conventionalNameToEmoji = {
+    build: '👷',
+    chore: '🧹',
+    ci: '🤖',
+    docs: '📝',
+    feat: '✨',
+    fix: '🐛',
+    perf: '⚡️',
+    refactor: '♻️',
+    revert: '⏪',
+    style: '🎨',
+    test: '✅',
+  }
+
+  export type ConventionalType = keyof typeof conventionalNameToEmoji
+
+  /**
+   * Checks if a commit message is a conventional commit.
+   */
+  function isConventionalCommit(message: string): boolean {
+    // No capture groups or length limits, just a simple regex to check if the message matches the conventional commit format
+    // Check that type is one of the conventional types
+    const regex = /^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(\(.+\))?: .+/
+    return regex.test(message)
+  }
+
+  interface CommitMetadata {
+    type: keyof typeof conventionalNameToEmoji
+    scope: string
+    description: string
+  }
+
+  function extractCommitMetadata(message: string): CommitMetadata {
+    const regex = /^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(\(.+\))?: (.+)/
+    const match = regex.exec(message)
+    if (match === null) {
+      throw new Error(`Failed to parse commit message: ${message}`)
+    }
+    return {
+      type: match[1] as ConventionalType,
+      scope: match[2],
+      description: match[3],
+    }
+  }
+
+interface GitLog {
+    sha: string
+    message: string
+}
+
+/**
+ * Get the list of shas between the current and previous sha.
+ * Uses git log to print the sha and commit message header for each commit.
+ * @param currentSha
+ * @param previousSha
+ * @returns List of shas between the current and previous sha
+ */
+export async function gitLog(
+  currentSha: string,
+  previousSha: string
+): Promise<GitLog[]> {
+  let shas = ''
+  const result = await exec(
+    'git',
+    ['log', `${previousSha}..${currentSha}`, '--pretty="format:%H %s"'],
+    {
+      listeners: {
+        stdout: (data: Buffer) => {
+          shas += data.toString()
+        }
+      }
+    }
+  )
+
+  if (result !== 0) {
+    throw new Error('Failed to get git log')
+  }
+
+  const commits = shas.split('\n').map(commit => {
+    const [sha, message] = commit.split(' ')
+    return { sha, message }
+  })
+
+  return commits
+}
+
+interface ActionMetadata {}
+
+export async function releaseSha(
+  octokit: Octokit,
+  context: Context,
+  environment: string
+): Promise<string> {
+  // Get deployment statuses for the selected environment
+  const deploymentStatuses = await octokit.rest.repos.listDeployments({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    environment
+  })
+
+  if (deploymentStatuses.status !== 200) {
+    throw new Error(`Failed to get deployments for environment ${environment}`)
+  }
+
+  let previousSha: string
+
+  // Find commit ref for the latest deployment, if any
+  if (deploymentStatuses.data.length > 0) {
+    const latestDeployment = deploymentStatuses.data[0]
+    previousSha = latestDeployment.sha
+  } else {
+    previousSha = github.context.sha
+  }
+
+  return previousSha
+}
+
+export async function processCommits(commits: GitLog[], workspace: string): Promise<CommitMetadata[]> {
+    let metadataList: CommitMetadata[] = []
+
+    // Checkout commit using shell script
+    for (const commit of commits) {
+
+      let result = ''
+      const exitCode = await exec('pnpm', ['turbo', 'run', 'build', `--filter='${workspace}...[${commit.sha}^1]'`, '--dry=json'], {
+        listeners: {
+          stdout: (data: Buffer) => {
+            result += data.toString()
+          }
+        }
+      })
+
+      if(exitCode !== 0) {
+        continue
+      }
+
+      // Parse output and see if commit affects workspace
+      const json = JSON.parse(result) as DryRunJson
+      const packages = json.packages
+      if (packages.includes(workspace) && isConventionalCommit(commit.message)) {
+        try {
+          const metadata = extractCommitMetadata(commit.message)
+          metadataList.push(metadata)
+        } catch (error) {}
+
+      }
+    }
+    return metadataList
+}
+
+export function groupCommits(commits: CommitMetadata[]): Record<keyof typeof conventionalNameToEmoji, CommitMetadata[]> {
+    return commits.reduce((acc, metadata) => {
+        if (!acc[metadata.type]) {
+          acc[metadata.type] = []
+        }
+        acc[metadata.type].push(metadata)
+        return acc
+      }, {} as Record<keyof typeof conventionalNameToEmoji, CommitMetadata[]>)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
+import { format } from 'date-fns'
+import { ConventionalType, conventionalNameToEmoji, gitLog, groupCommits, processCommits, releaseSha } from './helpers'
+
 
 /**
  * The main function for the action.
@@ -7,18 +10,50 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // Get some initial context and inputs necessary for the action
+    const owner = github.context.repo.owner
+    const repo = github.context.repo.repo
+    const environment: string = core.getInput('github-environment', { required: true })
+    const prefix: string = core.getInput('prefix', { required: true })
+    const token: string = core.getInput('github-token', { required: true })
+    const workspace: string = core.getInput('workspace', { required: true })
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const octokit = github.getOctokit(token)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const date = new Date()
+    const releaseTitle = `${prefix}-${format(date, 'yyyy-MM-dd-HH-mm')}`
+
+    // Process all commits since the last release
+    const previousSha = await releaseSha(octokit, github.context, environment)
+    const commits = await gitLog(github.context.sha, previousSha)
+    const metadataList = await processCommits(commits, workspace)
+
+    // Finally group metadata by type and create a pretty release body
+    const groupedMetadata = groupCommits(metadataList)
+
+
+    const releaseBody = Object.entries(groupedMetadata).map(([type, metadataList]) => {
+      const emoji = conventionalNameToEmoji[type as ConventionalType] // Object.entries trashes the type
+      const metadataLines = metadataList.map(metadata => `- ${metadata.description}`)
+      return `${emoji} **${type}**\n\n${metadataLines.join('\n')}`
+    }).join('\n\n\n')
+
+    // Create a release
+    const release = await octokit.rest.repos.createRelease({
+      owner,
+      repo,
+      tag_name: releaseTitle,
+      name: releaseTitle,
+      body: releaseBody,
+      draft: false,
+      prerelease: false,
+    })
+
+    // Add release URL as an output
+    core.setOutput('release-url', release.data.html_url)
+    core.setOutput('release-title', release.data.name)
+    core.setOutput('release-body', release.data.body)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
